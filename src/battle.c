6eb4f8f68c698065c17d32ce03cb86254b312c54
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-09-20 00:48:34 -0300

    feat: paralised-implementation

--- src/battle.c
+++ src/battle.c
@@ -82,16 +82,59 @@ int iHandlePlayerActionByCard(PSTRUCT_CARD pstCard, PSTRUCT_MONSTER pastMonsters
         
       vPrintLine("Voce se curou.", INSERT_NEW_LINE);
       break;
+    case CARD_PARALIZE:
+      // deixar o monster sem jogar por x turnos
+      // rolar um dado de 1 a 4 para definir o x turnos
+
+      int iRoundsParalized = 0;
+      int iMonsterTarget = 0;
+      int iDebuffCt = 0;
+      char szLine[63];
+      char* pszRoundStr = "rodadas";
+      
+      time_t lTime;
+  
+      time(&lTime);
+      srand(lTime);
+
+      iRoundsParalized = (rand() % 2) + 2;
+
+      if ( iAliveMonsterQty(pastMonsters, iMonsterCt) == 1) {
+        iMonsterTarget = iGetFirstAliveMonster(pastMonsters, iMonsterCt);
+      } else {
+        iMonsterTarget = iSelectMonsterFromList(iMonsterCt);
+      }
+
+      iDebuffCt = pastMonsters[iMonsterTarget].iDebuffCt;
+
+      pastMonsters[iMonsterTarget].astDebuff[iDebuffCt].iType = DEBUFF_TYPE_PARALYZE;
+      pastMonsters[iMonsterTarget].astDebuff[iDebuffCt].iDamage = pstCard->iValue;
+      pastMonsters[iMonsterTarget].astDebuff[iDebuffCt].iRounds = iRoundsParalized;
+      pastMonsters[iMonsterTarget].iDebuffCt ++;
+
+      if ( iRoundsParalized == 1 )
+        pszRoundStr = "rodada";
+
+      memset(szLine, 0, sizeof(szLine));
+      snprintf(szLine, sizeof(szLine),
+        "Voce aplicou paralisia a o monstro %d por %d %s",
+        iMonsterTarget,
+        iRoundsParalized,
+        pszRoundStr
+      );
+      vPrintLine(szLine, INSERT_NEW_LINE);
+      vSleepSeconds(2);
+      vTraceMonsters(pastMonsters, iMonsterCt);
+      break;
     default:
-      return CARD_NULL;
+      return CARD_NONE;
 
     vSleepSeconds(1);
   }
-  return 0;
+  return pstCard->iType;
 }
 
-void vPlayCard(int iCardIndex, PSTRUCT_DECK pstDeck, PSTRUCT_MONSTER pastMonsters, int iMonsterCount)
-{
+void vPlayCard(int iCardIndex, PSTRUCT_DECK pstDeck, PSTRUCT_MONSTER pastMonsters, int iMonsterCount){
   PSTRUCT_CARD pstCard;
 
   if (iCardIndex < 0 || iCardIndex > pstDeck->iHandCount)
@@ -108,7 +151,7 @@ void vPlayCard(int iCardIndex, PSTRUCT_DECK pstDeck, PSTRUCT_MONSTER pastMonster
     return;
   }
   
-  if ( iHandlePlayerActionByCard(pstCard, pastMonsters, iMonsterCount) < 0 ) 
+  if ( iHandlePlayerActionByCard(pstCard, pastMonsters, iMonsterCount) == CARD_NONE ) 
     return;
   
   vSleepSeconds(2);
